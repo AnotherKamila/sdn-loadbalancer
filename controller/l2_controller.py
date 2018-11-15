98ac1a77fcc58770dc3933d9504426c439b0b0ce
plochers <plochers@ethz.ch>
2018-11-15 16:06:32 +0000

    removed broadcast function

--- controller/l2_controller.py
+++ controller/l2_controller.py
@@ -21,48 +21,11 @@ class L2Controller(object):
         self.controller.reset_state()
         self.add_boadcast_groups()
         self.add_mirror()
-        #self.fill_table_test()
 
     def add_mirror(self):
 
         if self.cpu_port:
             self.controller.mirroring_add(100, self.cpu_port)
-
-    def add_boadcast_groups(self):
-
-        interfaces_to_port = self.topo[self.sw_name]["interfaces_to_port"].copy()
-        #filter lo and cpu port
-        interfaces_to_port.pop('lo', None)
-        interfaces_to_port.pop(self.topo.get_cpu_port_intf(self.sw_name), None)
-
-        mc_grp_id = 1
-        rid = 0
-        for ingress_port in interfaces_to_port.values():
-
-            port_list = interfaces_to_port.values()[:]
-            del(port_list[port_list.index(ingress_port)])
-
-            #add multicast group
-            self.controller.mc_mgrp_create(mc_grp_id)
-
-            #add multicast node group
-            handle = self.controller.mc_node_create(rid, port_list)
-
-            #associate with mc grp
-            self.controller.mc_node_associate(mc_grp_id, handle)
-
-            #fill broadcast table
-            self.controller.table_add("broadcast", "set_mcast_grp", [str(ingress_port)], [str(mc_grp_id)])
-
-            mc_grp_id +=1
-            rid +=1
-
-    def fill_table_test(self):
-        self.controller.table_add("dmac", "forward", ['00:00:0a:00:00:01'], ['1'])
-        self.controller.table_add("dmac", "forward", ['00:00:0a:00:00:02'], ['2'])
-        self.controller.table_add("dmac", "forward", ['00:00:0a:00:00:03'], ['3'])
-        self.controller.table_add("dmac", "forward", ['00:00:0a:00:00:04'], ['4'])
-
 
     def learn(self, learning_data):
 
